b7d201b262509632c6170a11aae2b1addfef893b
MaSkAoS <atilita@gmail.com>
2015-12-14 23:07:31 +0100

    Update onservice.lua

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -3,17 +3,18 @@ do
 local function run(msg, matches)
 local bot_id = our_id -- your bot id
    -- like local bot_id = 1234567
-    if matches[1] == 'leave' and is_admin(msg) then
+    if matches[1] == 'adios' and is_admin(msg) then
+        send_large_msg("chat#id"..msg.to.id, 'Hasta luego.', ok_cb, false)
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
-       send_large_msg("chat#id"..msg.to.id, 'this is not one of my groups.', ok_cb, false)
+       send_large_msg("chat#id"..msg.to.id, 'Este no es mi grupo ... Adios.', ok_cb, false)
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     end
 end
  
 return {
   patterns = {
-    "^[!/](leave)$",
+    "^[!/](adios)$",
     "^!!tgservice (.+)$",
   },
   run = run
