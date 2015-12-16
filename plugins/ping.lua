04d1d26474d8dffefdb3bfe2f6014d75e6c693dc
MaSkAoS <atilita@gmail.com>
2015-12-16 02:30:04 +0100

    Create ping.lua

---
+++ plugins/ping.lua
@@ -0,0 +1,14 @@
+ -- Actually the simplest plugin ever!
+
+local triggers = {
+	'^/ping[@'..bot.username..']*'
+}
+
+local action = function(msg)
+	sendMessage(msg.chat.id, '🎾Pong!')
+end
+
+return {
+	action = action,
+	triggers = triggers
+}
