a1f8115613fc7a445177c47a7ccf75191ed2f679
MaSkAoS <atilita@gmail.com>
2016-01-11 20:23:17 +0100

    Update antilink.lua

--- plugins/antilink.lua
+++ plugins/antilink.lua
@@ -1,10 +1,11 @@
 local function run(msg)
     if not is_momod(msg) then
         chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
-        return 'No links ... Kicked'
+        return 'No links ... Kick Spammer'
     end
 end
 
 return {patterns = {
     "https://telegram.me/joinchat/",
+    "http://telegram.me/joinchat/",
     }, run = run}
